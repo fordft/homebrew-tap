a86d8c8ae678db78359d395e3c57ece13b1f91df
Thanakrit Wongkha <fordzaaagt@gmail.com>
2026-09-11 19:06:47 +0000

    sui-ai 0.3.0 (cargo-dist generated; sha256 filled at release)

---
+++ Formula/sui-ai.rb
@@ -0,0 +1,68 @@
+class SuiAi < Formula
+  desc "cache-first multi-agent coding harness"
+  homepage "https://github.com/fordft/sui"
+  version "0.3.0"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/fordft/sui/releases/download/v0.3.0/sui-aarch64-apple-darwin.tar.xz"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/fordft/sui/releases/download/v0.3.0/sui-x86_64-apple-darwin.tar.xz"
+    end
+  end
+  if OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/fordft/sui/releases/download/v0.3.0/sui-aarch64-unknown-linux-gnu.tar.xz"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/fordft/sui/releases/download/v0.3.0/sui-x86_64-unknown-linux-gnu.tar.xz"
+    end
+  end
+
+  BINARY_ALIASES = {
+    "aarch64-apple-darwin": {},
+    "aarch64-unknown-linux-gnu": {},
+    "x86_64-apple-darwin": {},
+    "x86_64-unknown-linux-gnu": {}
+  }
+
+  def target_triple
+    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
+    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
+
+    "#{cpu}-#{os}"
+  end
+
+  def install_binary_aliases!
+    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
+      dests.each do |dest|
+        bin.install_symlink bin/source.to_s => dest
+      end
+    end
+  end
+
+  def install
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install "sui", "sui-certify", "sui-mission"
+    end
+    if OS.mac? && Hardware::CPU.intel?
+      bin.install "sui", "sui-certify", "sui-mission"
+    end
+    if OS.linux? && Hardware::CPU.arm?
+      bin.install "sui", "sui-certify", "sui-mission"
+    end
+    if OS.linux? && Hardware::CPU.intel?
+      bin.install "sui", "sui-certify", "sui-mission"
+    end
+
+    install_binary_aliases!
+
+    # Homebrew will automatically install these, so we don't need to do that
+    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
+    leftover_contents = Dir["*"] - doc_files
+
+    # Install any leftover files in pkgshare; these are probably config or
+    # sample files.
+    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
+  end
+end
